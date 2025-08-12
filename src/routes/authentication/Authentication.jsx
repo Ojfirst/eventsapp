422ac744f97083012ba9cd35bf380e506580e55c
Simon <omojide.o.j@gmail.com>
2025-08-13 00:16:55 +0100

    UI: Add Modal overlay for login and logout

--- src/routes/authentication/Authentication.jsx
+++ src/routes/authentication/Authentication.jsx
@@ -1,7 +1,25 @@
-import AuthForm from "../../components/Auth/AuthForm";
+import { useState } from 'react';
+
+import AuthForm from '../../components/Auth/AuthForm';
+import Modal from '../../components/UI/Modal';
 
 function AuthenticationPage() {
-  return <AuthForm />;
+	const [showModal, setShowModal] = useState(true);
+
+	const removeModalHandler = () => {
+		setShowModal(false);
+	};
+
+	return (
+		<>
+			{showModal && (
+				<Modal onClose={removeModalHandler}>
+					<AuthForm  />
+
+				</Modal>
+			)}
+		</>
+	);
 }
 
 export default AuthenticationPage;
