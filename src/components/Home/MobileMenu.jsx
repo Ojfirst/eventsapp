d26fef16f82fdc4a7d61b510bff0a4f8d248bb06
Simon <omojide.o.j@gmail.com>
2025-08-31 22:18:05 +0100

    UI: Optimize home page for mobile use

---
+++ src/components/Home/MobileMenu.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router';
+import classes from './MobileMenu.module.css';
+
+const MobileMenu = () => {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div className={classes.header}>
+			<div className={classes.logo}>
+				<i className="fas fa-calendar-star"></i>
+				Eventify
+			</div>
+			<nav className={classes.mobileNav}>
+				<button
+					className={classes.hamburger}
+					aria-label="Open menu"
+					onClick={() => setOpen((prev) => !prev)}>
+					<span className={open ? classes.open : ''}></span>
+					<span className={open ? classes.open : ''}></span>
+					<span className={open ? classes.open : ''}></span>
+				</button>
+				<div className={`${classes.menu} ${open ? classes.show : ''}`}>
+					<NavLink to="/" onClick={() => setOpen(false)}>
+						Home
+					</NavLink>
+					<NavLink to="/events" onClick={() => setOpen(false)}>
+						Events
+					</NavLink>
+					<NavLink to="/newsletter" onClick={() => setOpen(false)}>
+						Newsletter
+					</NavLink>
+					<NavLink to="/profile" onClick={() => setOpen(false)}>
+						Profile
+					</NavLink>
+					<NavLink to="/auth?mode=login" onClick={() => setOpen(false)}>
+						Login
+					</NavLink>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default MobileMenu;
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Outlet } from 'react-router';
 
 import MainNavigation from '../components/Home/MainNavigation';
+import MobileMenu from '../components/Home/MobileMenu';
+import { useIsMobile } from '../components/Home/useIsMobile';
 import Footer from '../components/Home/Footer';
+
 const RootLayer = () => {
-	return (
-		<>
-			<MainNavigation />
-			<main style={{'minHeight': '100vh'}}>
-				<Outlet />
-			</main>
-			<Footer />
-		</>
-	);
+  const isMobile = useIsMobile();
+  return (
+    <>
+      {isMobile ? <MobileMenu /> : <MainNavigation />}
+      <main style={{ minHeight: '100vh' }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default RootLayer;
